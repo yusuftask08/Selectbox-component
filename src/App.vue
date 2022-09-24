a2cf49fc44119ae94b5db2624ee453a0fae56152
yusuftask08 <yusuftaskiran1@egmail.com>
2022-09-24 18:07:41 +0300

    added selectbox component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <h3>Select Box Component</h3>
+    <SelectBox :data="data" />
   </div>
 </template>
 
 <script>
-
+import SelectBox from './Components/Selectbox/index.vue'
+import data from "./data/data.json";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components:{
+    SelectBox
+  },
+  data() {
+    return {
+      data: data,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
